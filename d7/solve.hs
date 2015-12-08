746c1179b9af50a5df564e37209c01d56c312cfb
Jeremagician <jeremagician@gmail.com>
2015-12-08 22:25:13 +0100

    cleanup d7, definetly don't like this solution

--- d7/solve.hs
+++ d7/solve.hs
@@ -17,24 +17,14 @@ instance Show Signal where
   show (Unop _ x) = "UNOP " ++ show x
   show (Binop x _ y) = show x ++ " BINOP " ++ show y
 
-instance Eq Signal where
-  Val x == Val y = x == y
-  Wire x == Wire y = x == y
-  Unop f x == Unop g y = x == y
-  Binop x f y == Binop z g w = x == z && y ==w
-  _ == _ = False
-
 -- Parsing stuff
 int :: Parser Word16
 int = read <$> many1 digit
 
 str = many1 lower
-
 ws = many (oneOf " ")
 lexeme p = ws *> p <* ws
-
 unop = (string "NOT" *> return complement)
-
 sl x y = shiftL x (fromIntegral y)
 sr x y = shiftR x (fromIntegral y)
 
@@ -43,12 +33,10 @@ binop =  (string "OR" *> return (.|.))
          <|> (string "LSHIFT" *> return sl)
          <|> (string "RSHIFT" *> return sr)
 
-atom = try (Val <$> int)
-       <|> (Wire <$> str)
+atom = try (Val <$> int) <|> (Wire <$> str)
 
 signal = try (Binop <$> atom <*> lexeme binop <*> atom)
-         <|> (Unop <$> lexeme unop <*> atom)
-         <|> atom
+         <|> (Unop <$> lexeme unop <*> atom) <|> atom
 
 inst = do
   sig <- signal
@@ -59,15 +47,6 @@ inst = do
 booklet = many (inst <* char '\n')
 
 -- Circuit Assembly
-
--- Top down method, don't work, don't know why
--- I'll figure it out ... eventually
-solve wire booklet = solve' (Wire wire)
-  where solve' (Val v) = Just v
-        solve' (Wire w) =  find ((== w) . fst) booklet >>= (solve' . snd)
-        solve' (Unop f x)  = f <$> (solve' x)
-        solve' (Binop x f y)  =  f <$> (solve' x) <*> (solve' y)
-
 -- Bottom up method
 apply repl (Wire x) = let xs = filter ((== x) . fst) repl
                        in if (length xs > 0)
