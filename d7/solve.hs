ed1752afbed5c3839d3bdf94d605f1ad7cbd99d8
jeremagician <jeremagician@gmail.com>
2015-12-08 09:41:37 +0100

    day 7 complete

--- d7/solve.hs
+++ d7/solve.hs
@@ -19,6 +19,13 @@ instance Show Signal where
   show (Unop _ x) = "UNOP " ++ show x
   show (Binop x _ y) = show x ++ " BINOP " ++ show y
 
+instance Eq Signal where
+  Val x == Val y = x == y
+  Wire x == Wire y = x == y
+  Unop f x == Unop g y = x == y
+  Binop x f y == Binop z g w = x == z && y ==w
+  _ == _ = False
+
 -- Parsing stuff
 int :: Parser Word16
 int = read <$> many1 digit
@@ -54,16 +61,35 @@ inst = do
 booklet = many (inst <* char '\n')
 
 -- Circuit Assembly
+
+-- Top down method, don't work, don't know why
+-- I'll figure it out ... eventually
 solve wire booklet = solve' (Wire wire)
   where solve' (Val v) = Just v
         solve' (Wire w) =  find ((== w) . fst) booklet >>= (solve' . snd)
         solve' (Unop f x)  = f <$> (solve' x)
         solve' (Binop x f y)  =  f <$> (solve' x) <*> (solve' y)
 
-table booklet =  [x | x@(_, Val _) <- booklet]
+-- Bottom up method
+apply repl (Wire x) = let xs = filter ((== x) . fst) repl
+                       in if (length xs > 0)
+                          then snd (head xs)
+                          else Wire x
+apply repl (Unop f (Val x)) = Val (f x)
+apply repl (Unop f x) = Unop (f) (apply repl x)
+apply repl (Binop (Val x) f (Val y)) = Val (f x y)
+apply repl (Binop x f y) = Binop (apply repl x) (f) (apply repl y)
+apply repl x = x
+
+table :: [(String, Signal)] -> [(String, Signal)]
+table booklet = build [] booklet
+  where build last bk = let repl = [x | x@(_, Val _) <- bk]
+                        in if (length repl) == (length bk)
+                           then bk
+                           else build repl (map (fmap (apply repl)) bk)
 
 main = do
   args <- getArgs
   content <- readFile (args !! 0)
   let bklt = parse booklet "" content
-  putStrLn (show $ fmap table bklt)
+  putStrLn (show $ fmap (find ((== (args !! 1)) . fst) . table) bklt)
