860fae8e75b2de7cde40079f4e1c6262adf3e6ed
jeremagician <jeremagician@gmail.com>
2015-12-13 18:13:31 +0100

    day 13 solved

--- d13/solve.hs
+++ d13/solve.hs
@@ -4,6 +4,9 @@ import System.Environment
 import Data.List
 import Data.Tuple
 
+-- YET ANOTHER INEFICIENT SOLUTION
+-- ... WORKING ON IT
+
 parse_lines :: String -> ((String, String), Int)
 parse_lines ln = case (words (init ln)) of
   [x, "would", act, amnt, "happiness", "units",
@@ -14,15 +17,21 @@ parse_lines ln = case (words (init ln)) of
 parse :: String -> [((String, String), Int)]
 parse = map parse_lines . lines
 
-topology_happiness cs xs = sum $ map (\x -> (cs M.! x) + (cs M.! (swap x))) pairs
+score cs (x,y) = if x == "me" || y == "me"
+                 then 0
+                 else (cs M.! (x,y)) + (cs M.! (y,x))
+
+topology_happiness cs xs = sum $ map (score cs) pairs
   where pairs = zip xs ((drop 1 xs) ++ (take 1 xs))
 
-solve cstrs = maximum $ map (topology_happiness cstrs) topologies
-  where people = nub $ map fst (M.keys cstrs)
-        topologies = permutations people
+solve cstrs people = maximum $ map (topology_happiness cstrs) topologies
+  where topologies = permutations people
 
 main = do
   args <- getArgs
   content <- readFile (args !! 0)
   let cstrs = M.fromList $ parse content
-  putStrLn $ show $ solve cstrs
+  let people = nub $ map fst (M.keys cstrs)
+  putStrLn $ "Part 1: " ++ (show $ solve cstrs people)
+  let everybody = "me":people
+  putStrLn $ "Part 2: " ++ (show $ solve cstrs everybody)
