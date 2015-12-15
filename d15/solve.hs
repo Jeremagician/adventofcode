d51b0a7811136847c07c89faa2972aebe67e0e63
jeremagician <jeremagician@gmail.com>
2015-12-15 13:38:48 +0100

    less bruteforcy for day 15

--- d15/solve.hs
+++ d15/solve.hs
@@ -11,15 +11,17 @@ parse_line ln = case words (filter (/=',') ln) of
   _ -> error $ "Bad input line: \"" ++ ln ++"\""
 
 
--- too bruteforcy, find a better way to generate all this
-spoon_combination :: Int -> [[Integer]]
-spoon_combination l = filter ((== 100) . sum) $ replicateM l [1..99]
+spoon_combinations :: Integer -> Integer -> [[Integer]]
+spoon_combinations 1 u = [[u]]
+spoon_combinations l u = do
+  x <- [1..((u-l)+1)]
+  xs <- spoon_combinations (l - 1)(u - x)
+  return (x:xs)
 
 pos x
   | x < 0 = 0
   | otherwise = x
 
--- [[ing1], [ing2]]
 partial_sum ing recipe = map (pos . sum) $ transpose melange
   where dosage = zip ing recipe
         melange = map (\(x, f) -> (map (*f) x)) dosage
@@ -27,7 +29,7 @@ partial_sum ing recipe = map (pos . sum) $ transpose melange
 --                                 Recipe score    Total Calories
 --                                     vvvvv         vvvvvv
 all_recipe_score ing = map (\x -> (product (init x), last x)) all_melanges
-  where all_recipes = spoon_combination $ length ing
+  where all_recipes = spoon_combinations (fromIntegral (length ing)) 100
         all_melanges = map (partial_sum ing) all_recipes
 
 main = do
